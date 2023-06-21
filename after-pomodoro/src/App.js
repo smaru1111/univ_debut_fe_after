eeaa29efbc4e228997806ec169e93725e14f5ebc
smaru1111 <96244711+smaru1111@users.noreply.github.com>
2023-06-21 19:23:45 +0900

    add swa func

--- after-pomodoro/src/App.js
+++ after-pomodoro/src/App.js
@@ -1,13 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      // APIコール
+      const { text } = await( await fetch(`/api/HttpTrigger1`)).json();
+      setData(text);
+    })();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. <br />
+          API Response Text: {data}
         </p>
         <a
           className="App-link"
